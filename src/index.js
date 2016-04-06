57d914caf9c17dda977fed947e26db73ffd31b5a
David Emory <demory@conveyal.com>
2016-04-06 13:27:17 -0400

    Move DatatoolsNavbar; version 0.2.0

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 module.exports = {
   Auth0Manager: require('./Auth0Manager'),
   DataManager: require('./DataManager'),
+  DatatoolsNavbar: require('./DatatoolsNavbar'),
   UserPermissions: require('./UserPermissions')
 }
